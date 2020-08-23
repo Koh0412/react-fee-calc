24bd2af61eba8eea1bb42a0f69625c5a00f3c43a
kohei.hujita <point.out92@gmail.com>
2020-08-24 04:40:17 +0900

    add warning

--- src/components/Summary.tsx
+++ src/components/Summary.tsx
@@ -6,19 +6,28 @@ type SummaryProps = {
 }
 
 class Summary extends React.Component<SummaryProps> {
+  get isOverTenThousand(): boolean {
+    return this.props.totalAmount > 10000;
+  }
+
   render() {
     return (
-      <section className="summary">
-        <div className="party">
-          <span className="party">{this.props.numOfPeople}</span>
-          <span>名様</span>
-        </div>
-        <div className="total-amount">
-          <span>合計</span>
-          <span className="total-amount">{this.props.totalAmount}</span>
-          <span>円</span>
-        </div>
-      </section>
+      <>
+        <section className={["summary", this.isOverTenThousand ? "warn" : "safety"].join(" ")}>
+          <div className="party">
+            <span className="party">{this.props.numOfPeople}</span>
+            <span>名様</span>
+          </div>
+          <div className="total-amount">
+            <span>合計</span>
+            <span className="total-amount">{this.props.totalAmount}</span>
+            <span>円</span>
+          </div>
+        </section>
+        {this.isOverTenThousand && (
+          <p>10000円を超えていますが、問題ないでしょうか？</p>
+        )}
+      </>
     )
   }
 }
